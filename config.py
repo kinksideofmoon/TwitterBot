5908a849aa67d8236c40c671259cbba135283f15
kinksideofmoon <pawel.wojtal@gmail.com>
2022-11-03 12:11:38 +0100

    Fixing small issues after initial commit

--- config.py
+++ config.py
@@ -2,18 +2,18 @@ import logging
 
 
 class TwitterConfig:
-    ClientID = 'WEdGSVh0Q2JsRGRwazRjTFI2TWI6MTpjaQ'
-    ClientSecret = 'M84XPa7zqEloiPq5jEBrmNpZoh2rPw0GLtSslhbMcUnEUmBmQA'
-    consumer_key = 'WVYpqfk1HWQyJkmW0gHdL3Eef'
-    consumer_secret = 'F3a5sauFEmmtMrJy29DSooVThttfIAOxjNEUmFOEywBDGRWH6O'
-    access_token = '3412500364-4nvgHRMF7QkWIn8qUxwXuxt54rDxOCRFRwweaDk'
-    access_secret = 'HEzL386HjQo1kpONPMxvZ6cd4sQQrEYCCleaVXs9h45jS'
+    ClientID = 'xxx'
+    ClientSecret = 'xxx'
+    consumer_key = 'xxx'
+    consumer_secret = 'xxx'
+    access_token = 'xxx'
+    access_secret = 'xxx'
 
 
 class PinterestConfig:
-    email = 'pawel.wojtal@gmail.com'
-    password = 'OperaGloves1^'
-    username = 'KinkSideOfTheMoon'
+    email = 'xxx'
+    password = 'xxx'
+    username = 'xxx'
 
 
 class LoggingConfig:
@@ -22,10 +22,10 @@ class LoggingConfig:
 
 
 class TelegramConfig:
-    api_id = '15194207'
-    api_hash = 'ae48649a5561b9f9d76163a5c22d8d35'
-    token = '5555538687:AAGH6R5PDfW8E8R50UDx5G3tjF3X6QGTdVU'
-    phone = '+48503114736'
+    api_id = 'xxx'
+    api_hash = 'xxx'
+    token = 'xxx'
+    phone = 'xxx'
 
 
 class Config:
