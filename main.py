2a0126ad8d004449b78ff1246af6107014c17870
kinksideofmoon <pawel.wojtal@gmail.com>
2023-01-04 16:06:48 +0100

    Change the number of posted images from 50 to 5

--- main.py
+++ main.py
@@ -87,6 +87,6 @@ def get_random_images_from_pinterest_and_tweet_them(number_of_image_to_be_posted
             extras.seep_with_progress_bar(240, "Waiting to not overload Twitter API...")
 
 
-get_random_images_from_pinterest_and_tweet_them(50, chosen_board)
+get_random_images_from_pinterest_and_tweet_them(5, chosen_board)
 
 telegram_client.send("...End")
